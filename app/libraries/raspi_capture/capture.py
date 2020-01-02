f4e4c65659d8a49d25072bb8403e071e53f1b2dc
matphilippe <matthew.m.philippe@gmail.com>
2020-01-02 14:18:50 +0100

    fixing camera things

--- app/libraries/raspi_capture/capture.py
+++ app/libraries/raspi_capture/capture.py
@@ -9,19 +9,18 @@ class CaptCamera:
 
     def __init__(self, gpio_list):
         self.gpio_handler = GPIOHandler(gpio_list)
-        self.camera = PiCamera()
-        self.raw_capture = PiRGBArray(self.camera)
 
     def capture_to_array(self, warmup=1):
-        self.gpio_handler.setON()
-        self.camera.start_preview()
-        time.sleep(warmup)
-        self.camera.capture(self.raw_capture)
-        self.camera.stop_preview()
-        image = self.raw_capture.array
+        with PiCamera() as camera:
+            self.gpio_handler.setON()
+            camera.start_preview()
+            raw_capture = PiRGBArray(camera)
+            camera.capture(raw_capture)
+            camera.stop_preview()
+            self.gpio_handler.setOFF()
+            time.sleep(warmup / 2)
 
-        self.gpio_handler.setOFF()
-        time.sleep(warmup / 2)
+        image = raw_capture.array
         return image
 
     def capture_to_file(self, file, warmup=1):
@@ -35,4 +34,5 @@ class CaptCamera:
             self.gpio_handler.setOFF()
             time.sleep(warmup / 2)
         my_file.close()
+
         return None
