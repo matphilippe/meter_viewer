2560f3e23a5c73782685fb17540c299738b35ac1
matphilippe <matthew.m.philippe@gmail.com>
2020-01-02 14:06:54 +0100

    modifying paths for file folder - againagain

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def send_capture():
     # 7 and 11 are the GPIO ports for leds.
     # We should give them as req args.
     raspi_handler = raspi.CaptCamera(gpio_list=[7, 11])
-    raspi_handler.capture_to_file('./temp/images/' + constants.CAPTURE_IMG)
+    raspi_handler.capture_to_file('app/temp/images/' + constants.CAPTURE_IMG)
     return send_from_directory('./temp/images', constants.CAPTURE_IMG)
 
 
